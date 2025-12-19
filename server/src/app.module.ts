db2c456d3a452568a948c3d28417e78b87646d9a
Александр Ягодкин <it4ban@it4banPC.local>
2025-12-19 22:00:39 +0300

    feat: add prisma and postgres database

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
+
+import { AuthModule } from './auth/auth.module';
 import { AppController } from './app.controller';
+import { PrismaService } from './prisma.service';
 import { AppService } from './app.service';
 
 @Module({
-	imports: [],
+	imports: [AuthModule],
 	controllers: [AppController],
-	providers: [AppService],
+	providers: [PrismaService, AppService],
 })
 export class AppModule {}
